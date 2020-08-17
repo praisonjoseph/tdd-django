d1e344b1066a692a2d3b752b226032d365c72743
Joseph, Praison <praison.joseph@gmail.com>
2020-08-17 09:49:42 +0530

    failed FT to check if it can take more than one list

--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render, HttpResponse
 
 def home_page(request):
     # return HttpResponse('<html> <title>To-Do lists</title> <h1>To-Do</h1> </html>')
+    if request.method == 'POST':
+        # print(request.POST)
+        # return HttpResponse(request.POST['item_text'])
+        context_dict = {
+            'new_item_text': request.POST['item_text']
+        }
+        return render(request, 'home.html', context=context_dict)
     return render(request, 'home.html')
