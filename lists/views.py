151941551b40ea31ac1c442cbd66469f3524b884
Joseph, Praison <praison.joseph@gmail.com>
2020-08-18 21:10:04 +0530

    redirect the POST and added the ability to display the list

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from lists.models import Item
 
 # Create your views here.
 
 def home_page(request):
-    # return HttpResponse('<html> <title>To-Do lists</title> <h1>To-Do</h1> </html>')
     if request.method == 'POST':
-        # print(request.POST)
-        # return HttpResponse(request.POST['item_text'])
-        context_dict = {
-            'new_item_text': request.POST['item_text']
-        }
-        return render(request, 'home.html', context=context_dict)
-    return render(request, 'home.html')
+        # new_item_text = request.POST['item_text']
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    # else:
+        # new_item_text = ''
+    # return render(request, 'home.html', {
+            # 'new_item_text': new_item_text
+        # })
+    qs = Item.objects.all()
+    return render(request, 'home.html', {'items' : qs})
